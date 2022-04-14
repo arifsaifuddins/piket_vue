e14229a089b00c0b0c0326e6245118aab793b77b
saifuddien <ariefsaifuddien01@gmail.com>
2022-04-15 01:45:32 +0200

    ok

--- src/components/Surah.vue
+++ src/components/Surah.vue
@@ -76,13 +76,13 @@
         {{ arab }}
       </h1>
     </div>
-    <div class="flex flex-col">
+    <div class="flex flex-col mt-4">
       <h1
         v-if="pre != null"
         class="
           font-[DroidarabickufiRegular]
-          mt-4
-          text-xl text-center
+          mb-4
+          text-lg text-center
           pb-4
           border-b
           dark:border-slate-200
@@ -93,22 +93,30 @@
       >
         {{ basmalah }}
       </h1>
-      <h1
+      <div
         v-for="ayat in surah"
         :key="ayat.number.inSurah"
         class="
-          text-3xl
-          mt-3
-          text-right
-          dark:bg-slate-900
-          font-[ScheherazadeRegular]
-          bg-white
-          p-2
+          flex flex-col
           rounded-xl
+          mb-3
+          overflow-hidden
+          bg-slate-100
+          dark:bg-slate-800
         "
       >
-        {{ ayat.text.arab }}
-      </h1>
+        <h1
+          class="text-2xl text-right font-bold font-[ScheherazadeRegular] p-4"
+        >
+          {{ ayat.text.arab }}
+        </h1>
+        <h1 class="text-sm p-4 border-t dark:border-slate-200 border-slate-800">
+          {{ ayat.number.inSurah }} - {{ ayat.translation.id }}
+        </h1>
+        <audio controls>
+          <source :src="`${ayat.audio.primary}.mp3`" type="audio/mpeg" />
+        </audio>
+      </div>
     </div>
   </div>
 </template>
