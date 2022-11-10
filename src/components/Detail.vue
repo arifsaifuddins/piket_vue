54243beaca75d1e02c7cc457f59c2a25e7e0ad32
saifuddien <ariefsaifuddien01@gmail.com>
2022-11-10 19:51:15 +0200

    oke

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,17 +1,16 @@
 <script setup>
-  import json from "../../public/json/anggota.json";
-  import { useRouter } from "vue-router";
+import json from "../../public/json/anggota.json";
+import { useRouter } from "vue-router";
 
-  const router = useRouter();
+const router = useRouter();
 
-  const date = new Date();
-  const year = date.getFullYear();
+const date = new Date();
+const year = date.getFullYear();
 </script>
-
+v
 <template>
   <div class="flex flex-col">
-    <div
-      class="
+    <div class="
         flex
         items-center
         fixed
@@ -25,35 +24,22 @@
         sm:w-[400px]
         mx-auto
         w-full
-      "
-    >
+      ">
       <div class="text-xl flex justify-start items-center">
-        <i
-          class="fa fa-arrow-left-long mr-2 cursor-pointer text-xl"
-          @click="router.back()"
-        ></i>
-        <img
-          :src="`/assets/img/${json[0][$route.params.id].icon}.jpg`"
-          alt="icon"
-          class="w-8 h-8 rounded-full mr-2"
-        />
+        <i class="fa fa-arrow-left-long mr-2 cursor-pointer text-xl" @click="router.back()"></i>
+        <img :src="`/assets/img/${json[0][$route.params.id].icon}.jpg`" alt="icon" class="w-8 h-8 rounded-full mr-2" />
         {{ json[0][$route.params.id].nama }}
       </div>
     </div>
     <div class="p-6 pt-32 text-center">
-      <img
-        :src="`/assets/img/${json[0][$route.params.id].icon}.jpg`"
-        alt="icon"
-        class="w-48 h-48 rounded-full mx-auto"
-      />
+      <img :src="`/assets/img/${json[0][$route.params.id].icon}.jpg`" alt="icon"
+        class="w-48 h-48 rounded-full mx-auto" />
       <h1 class="mt-6 mb-4 text-3xl font-extrabold">
         {{ json[0][$route.params.id].nama }}
       </h1>
       <div>{{ json[0][$route.params.id].no }}</div>
     </div>
-    <hr
-      class="border-b-1 w-[98%] mx-auto dark:border-slate-200 border-slate-800"
-    />
+    <hr class="border-b-1 w-[98%] mx-auto dark:border-slate-200 border-slate-800" />
     <h1 class="mt-3 p-2 flex items-start text-xl font-bold">
       <div class="w-28 text-[#f59800]">Daerah</div>
       <div>: {{ json[0][$route.params.id].daerah }}</div>
@@ -74,10 +60,7 @@
       <div class="w-28 inline text-[#f59800]">Angkatan</div>
       <div>: {{ json[0][$route.params.id].angkatan }} - {{ year }}</div>
     </h1>
-    <a
-      target="_blank"
-      :href="`https://wa.me//${json[0][$route.params.id].no}`"
-      class="
+    <a target="_blank" :href="`https://wa.me//${json[0][$route.params.id].no}`" class="
         w-full
         my-4
         bg-[#f59800]
@@ -89,8 +72,6 @@
         rounded-lg
         text-center
         dark:bg-slate-700
-      "
-      ><i class="fab fa-whatsapp"></i> Whatsapp</a
-    >
+      "><i class="fab fa-whatsapp"></i> Whatsapp</a>
   </div>
 </template>
--- src/components/Tentang.vue
+++ src/components/Tentang.vue
@@ -1,6 +1,6 @@
 <script setup>
-  import Layout from "./Layout.vue";
-  import json from "../../public/json/tech.json";
+import Layout from "./Layout.vue";
+import json from "../../public/json/tech.json";
 </script>
 
 <template>
@@ -10,17 +10,14 @@
         <img src="/assets/icons/icon.png" alt="icon" class="w-32 mx-auto" />
         <h1 class="mt-5 text-3xl font-extrabold">Piket Sabtu</h1>
       </div>
-      <hr
-        class="
+      <hr class="
           border-b-1
           w-[98%]
           mx-auto
           dark:border-slate-200
           border-slate-800
-        "
-      />
-      <div
-        class="
+        " />
+      <div class="
           w-full
           p-3
           px-4
@@ -31,59 +28,30 @@
           justify-between
           items-center
           dark:bg-slate-700
-        "
-      >
+        ">
         <h1 class="font-bold text-xl text-[#f59800]">
           Dibuat dengan Teknologi :
         </h1>
       </div>
 
-      <a
-        v-for="tech in json[0]"
-        target="_blank"
-        :href="tech.url"
-        :key="tech.name"
-      >
+      <a v-for="tech in json[0]" target="_blank" :href="tech.url" :key="tech.name">
         <div class="p-3 pt-6 text-lg">
-          <img
-            :src="'/assets/logo/' + tech.icon + '.png'"
-            alt="icon"
-            class="w-8 h-8 inline mr-3"
-          />{{ tech.name }}
+          <img :src="'/assets/logo/' + tech.icon + '.png'" alt="icon" class="w-8 h-8 inline mr-3" />{{ tech.name }}
         </div>
-        <hr
-          class="
+        <hr class="
             border-b-1
             w-[98%]
             mx-auto
             dark:border-slate-200
             border-slate-800
-          "
-        />
+          " />
       </a>
 
-      <div
-        class="
-          w-full
-          p-3
-          mt-20
-          shadow-md
-          rounded-lg
-          flex
-          justify-center
-          items-center
-          dark:bg-slate-700
-        "
-      >
+      <div class=" w-full p-3 mt-20 shadow-md rounded-lg flex justify-center items-centser dark:bg-slate-700">
         <h1 class="font-bold text-xl text-[#f59800]">Temukan kami di :</h1>
       </div>
       <div class="my-10 w-full flex justify-around items-center">
-        <a
-          v-for="sosial in json[1]"
-          target="_blank"
-          :href="sosial.url"
-          :key="sosial.name"
-        >
+        <a v-for="sosial in json[1]" target="_blank" :href="sosial.url" :key="sosial.name">
           <i :class="`fab fa-${sosial.name} text-3xl`"></i>
         </a>
       </div>
