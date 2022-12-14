2e1b5f0eeb117653965eb6b6d47bb008258e869f
Arief Saifuddien <ariefsaifuddien01@gmail.com>
2022-12-14 11:09:45 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Menus from "./components/Menus.vue";
 import Menu from "./components/Menu.vue";
